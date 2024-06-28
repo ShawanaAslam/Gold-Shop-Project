82a6b98eda7ce7774db4fdbcd9130751e8d2769b
Shawana Aslam <shawanaaslam412@gmail.com>
2024-06-28 16:13:48 +0500

    Code Upload

---
+++ lib/controller/constants/linker/linker.dart
@@ -0,0 +1,13 @@
+export 'package:flutter/material.dart';
+export 'package:get/get.dart';
+export 'package:get/get_core/src/get_main.dart';
+export 'package:goldshop_project/controller/constants/widgets/reusable_mytxtformcon.dart';
+export 'package:goldshop_project/controller/constants/widgets/reusable_text.dart';
+
+export 'package:get/get_navigation/src/root/get_material_app.dart';
+export 'package:goldshop_project/view/Project%20of%20Gold%20Shop.dart';
+
+export 'package:goldshop_project/controller/constants/widgets/reusable_container.dart';
+export 'package:goldshop_project/controller/constants/widgets/reusable_simpletext.dart';
+
+
---
+++ lib/controller/constants/widgets/reusable_simpletext.dart
@@ -0,0 +1,19 @@
+
+
+import '../linker/linker.dart';
+
+class SimpleText extends StatelessWidget {
+  String txt;
+  double font;
+  FontWeight fntwt;
+  SimpleText({super.key,required this.txt,required this.font,required this.fntwt});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text( txt,
+      style: TextStyle(
+          color: Colors.black,
+          fontSize: font,
+          fontWeight: fntwt));
+  }
+}
